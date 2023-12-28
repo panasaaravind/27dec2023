35ce6ae595e79993c02277ba1b492111ff4825e3
panasaaravind0 <panasaaravind0@gmail.com>
2023-12-28 18:49:01 +0530

    js data types

---
+++ js/script.js
@@ -0,0 +1,11 @@
+
+var student1 = {
+    // 1.property:value
+    name:"aravind",
+    surname:"panasa"
+    //2.
+};//js object
+
+//the frist way to see the output of the js'
+//object.property name
+console.log("hello "+student1.name);    //+ is a concatination operator
---
+++ js/script2.js
@@ -0,0 +1,9 @@
+
+let student4 = {
+    //property:value
+    name:"shivalingam",
+    surname:"katna"
+};  //js object
+//the 4th way to see the js code
+document.write("<div>"+student4.name+"</div>");
+
